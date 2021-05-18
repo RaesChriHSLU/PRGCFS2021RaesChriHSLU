6edee042fada2c0e9fefd62bbb9d6ece5bfefc9a
prgc <christoph.raess@hslu.stud.ch>
2021-05-18 10:46:58 +0200

    added my main-function template

--- 40_Exercises/00_Diverses/PRGCTestat.c
+++ 40_Exercises/00_Diverses/PRGCTestat.c
@@ -4,133 +4,120 @@
 #define max_recs 1940
 
 
-
-
-	//Define struct in the same structure as the records
+//Hochschule Luzern, Maschinenbau, Programmieren in C, FS2021, Testat, Christoph Raess
+//------------------------------------------------------------------------------------------------------
+	//Struktur soll genau gleich definiert werden wie die Sturktur der Aufnahmen
+	
+	
 	struct rec {
-		long long time;
-		int pressure;
-		char system;
-		char alarm;
+	
+	
+				long long time;
+				int pressure;
+				char system;
+				char alarm;
+	
+	
 		
 	};
 	
-	int main()
-	{
-		//Read the data out of the binfile
-		struct rec reactor_record[max_recs];
-		int i;
-		
-		FILE *ptr_binfile;
-		ptr_binfile=fopen("pressureSpike.bin", "rb");
-		
-		if(!ptr_binfile)
+	
+	
+	//Hauptprogramm----------------------------------------------------------------------------------
+	
+		int main()
 		{
+		
+			//Lies die Daten aus dem binfile
+		
+		
+			struct rec Reaktor_Aufnahme[max_recs];
+			int c;
+		
+		
+			FILE *ptr_binfile;
+			ptr_binfile=fopen("pressureSpike.bin", "rb");
+		
+		
+			
+			if(!ptr_binfile)
+		
+		
+			{
 			printf("Unable to opten file!");
 			return 1;
-		}
+			}
 		
-		fseek(ptr_binfile, sizeof(struct rec), SEEK_SET);
-		rewind(ptr_binfile);
 		
-		for(i= 1; i < max_recs; i++) {
-			fread(&reactor_record[i] .time, sizeof(long long), 1, ptr_binfile);
-			fread(&reactor_record[i] .pressure, sizeof(int), 1, ptr_binfile);	
-			fread(&reactor_record[i] .system, sizeof(char), 1, ptr_binfile);
-			fread(&reactor_record[i] .alarm, sizeof(long long), 1, ptr_binfile);
+		
+			fseek(ptr_binfile, sizeof(struct rec), SEEK_SET);
+			rewind(ptr_binfile);
+		
+			for(c= 1; c < max_recs; c++) 
 			
+			
+			//Auflistung: Zeit, Druck, Systemstatus, Alarmsignal
+			{
+			fread(&Reaktor_Aufnahme[c] .time, sizeof(long long), 1, ptr_binfile);
+			fread(&Reaktor_Aufnahme[c] .pressure, sizeof(int), 1, ptr_binfile);	
+			fread(&Reaktor_Aufnahme[c] .system, sizeof(char), 1, ptr_binfile);
+			fread(&Reaktor_Aufnahme[c] .alarm, sizeof(long long), 1, ptr_binfile);	
+			}
+		
+	
+		
+			for(c=1; c < max_recs; c++) 
+			{
+			printf("Time: %lld\n" , Reaktor_Aufnahme[c] .time);
+			printf("Pressure: %d\n" , Reaktor_Aufnahme[c] .pressure);
+			printf("System State: %d\n" , Reaktor_Aufnahme[c] .system);
+			printf("Alarm State: %d\n" , Reaktor_Aufnahme[c] .alarm);	
+				
+			printf("\n");
+			}
+			
+		
+			fclose(ptr_binfile);
+		
+		
+		
+		
+	//CSV-File--------------------------------------------------------------------------------------
+			// Ab hier oeffne das CSV-FILE und schreibe die Daten hinein
+		
+			FILE *fp = NULL;
+			fp = fopen("reactorlogs.csv","a");
+		
+			if(fp==NULL)
+			
+			
+			{
+			printf("Error in creating the file\n");
+			exit(1);
+			}
+		
+		
+		
+			for(c=1; c< max_recs; c++)  {
+		
+			fprintf(fp, "%lld;%d,%d,%d\n", 
+			Reaktor_Aufnahme[c] .time, 
+			Reaktor_Aufnahme[c].pressure, 
+			Reaktor_Aufnahme[c].system, 
+			Reaktor_Aufnahme[c].alarm);
+			}
+		
+		
+		
+			fclose(fp);
+		
+		
+			return 0;
+		
+		
 		}
-		
-		
-		
-		for(i=1; i < max_recs; i++) {
-		printf("Time: %lld\n" , reactor_record[i] .time);
-		printf("Pressure: %d\n" , reactor_record[i] .pressure);
-		printf("System State: %d\n" , reactor_record[i] .system);
-		printf("Alarm State: %d\n" , reactor_record[i] .alarm);		
-		printf("\n");
-		}
-		
-		fclose(ptr_binfile);
-		
-		
-		//Open a csv file and write the data in it
-		
-		FILE *fp = NULL;
-		fp = fopen("reactorlogs.csv","a");
-		
-		if(fp==NULL)
-		{
-		printf("Error in creating the file\n");
-		exit(1);
-		}
-		
-		for(i=1; i< max_recs; i++)  {
-		fprintf(fp, "%lld;%d,%d,%d\n", reactor_record[i] .time, reactor_record[i].pressure, reactor_record[i].system, reactor_record[i].alarm);
-		}
-		
-		fclose(fp);
-		
-		
-		return 0;
-		
-		
-	}
 	
 	
-/*
-// fread_fwrite.c
-#include <stdio.h>
-#include <stdlib.h>
-
-struct {
-	long long time;
-	int pressure;
-	char system[4];
-	char alarm[1];
-}records;
-
-void speichern(void) {
-	FILE *save =fopen("pressureSpike.bin","r+b");
-	if( NULL== save) {
-		save = fopen("pressureSpike.bin", "w+b");
-		if( NULL==save )  {
-		fprintf(stderr, "Kann \"adressen.dat\" nicht öffen!\n);
-		return;
-		}
-	}
-	// FILE-Zeiger save auf das Ende der Datei setzen
-	fseek(save, 0, SEEK_END);
-	
-	//Wir schreiben eine Adresse ans Ende von "adressen.dat".
-	if(fwrite(%records, sizeof(records), 1, save)  !=1)  {
-		fprintf(stderr, "Fehler bei fwrite..!!!
-	}
-	//Wir geben unseren FILE-Zeiger wieder frei.
-	fcloese(save);
-}
-
-void ausgabe(void) {
-	FILE *output = fopen("pressureSpike.bin", "r+b");
-	if (NULL ==output ) {
-		fprintf(stderr, "Kann \"adressen.dat\" nicht öffnen!\n")
-		return;
-	}
-	//Wir lesen alle Adressen aus "pressureSpike.bin".
-	while( fread(&records, sizeof(records), 1, output) == 1) {
-	printf("time...........: %lld\n", records.time);
-	printf("pressure.......: &d\n", records.pressure);
-	printf("system.........: &s\n", records.system);
-	printf("alarm..........: &s\n", records.alarm);
-	printf("\n\n");
-	
-	}
-	fclose(output);
-}
-
-
-
-*/
 
 
 
